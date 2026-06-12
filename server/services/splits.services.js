792fe78b27a4429795daeb70b596ba0f5186105d
Aadish Sonawane <aadishpcs@gmail.com>
2026-06-13 00:00:07 +0530

    created files for splits

---
+++ server/services/splits.services.js
@@ -0,0 +1,13 @@
+// export const createSplitWithDays = async (userId, name) => {
+//   // insert into splits, then loop 7 times inserting into split_days
+//   // wrap in a transaction
+// }
+// export const getSplitsByUser = async (userId) => {...}
+// export const getSplitWithDetails = async (splitId) => {
+//   // nested query: split + split_days + split_day_exercises + exercises
+// }
+// export const updateSplit = async (splitId, name) => {...}
+// export const deleteSplit = async (splitId) => {...}
+// export const setActiveSplit = async (userId, splitId) => {
+//   // set all user's splits is_active=false, then this one =true
+// }
