0940019caf65df10bcd2fc00cfae1796cb0e0adf
Aadish Sonawane <aadishpcs@gmail.com>
2026-06-20 01:03:53 +0530

    fix(api): enforce user authorization on split days services

--- server/controllers/splitDays.controller.js
+++ server/controllers/splitDays.controller.js
@@ -3,9 +3,10 @@ import { updateSplitDayService } from "../services/splitDays.services.js";
 async function updateSplitDay(req, res) {
     const { id } = req.params;
     const { label, is_rest } = req.body;
+    const userId = req.user.id;
 
     try {
-        const result = await updateSplitDayService(id, label, is_rest);
+        const result = await updateSplitDayService(id, label, is_rest, userId);
         res.status(200).json({
             message: "Split day renamed succesfully",
             data: result,
